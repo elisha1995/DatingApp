1c40d7ae49b71b470a25ee1edfed393d6a7c34c3
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-17 17:35:07 +0000

    Update API and client: add CORS support in Program.cs, update database files, and integrate Tailwind CSS with DaisyUI in the client. Add user avatar image and enhance app layout in app.html.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8296ec98d60743976548d34872d63532a8345107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117db1ee865f40d12c484ff5afab55d257ac6670")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 export class App implements OnInit {
   private readonly http = inject(HttpClient);
   protected readonly title = signal('Dating App');
+  protected readonly members = signal<any>([]);
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.error(error);
-      },
-      complete: () => {
-        console.log('Request completed');
-      },
-    });
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
