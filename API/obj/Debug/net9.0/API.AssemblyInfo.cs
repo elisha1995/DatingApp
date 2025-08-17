297496e29959c58fd9aeb1dbf20d5b071c8da04e
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-17 02:18:09 +0000

    Add UsersController with endpoints to retrieve all users and a user by ID; update assembly informational version.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4032f1bb6e80cb31942f9be7b0475eb56b3ff65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d88007ac3364da7ec303336ee39efa563467460")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
