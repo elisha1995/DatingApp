d7b31c2325d636b5b61fd814213179e4dc6ed12e
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-19 11:02:35 +0000

    Add navigation component and account service: implement user login/logout functionality, enhance app layout with a navigation bar, and update Angular configuration for component schematics. Include new database files for dating app functionality.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4befd24931e6caf92140e027b5b254d4387978b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27f4a4df41909c57c7712cc7be4bb670dce5b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
+  imports: [Nav],
 })
 export class App implements OnInit {
   private readonly http = inject(HttpClient);
