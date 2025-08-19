0ad6b676ad4a11ddfce88baf419c791dfa5c46de
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-19 12:48:13 +0000

    Implement user registration and home components: add registration form and logic, enhance home page with registration toggle, and update navigation to display logged-in user's name. Modify account service for user management and adjust app structure for new features.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,25 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { User } from '../types/user';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Nav],
+  imports: [Nav, Home],
 })
 export class App implements OnInit {
+  private readonly accountService = inject(AccountService);
   private readonly http = inject(HttpClient);
   protected readonly title = signal('Dating App');
-  protected readonly members = signal<any>([]);
+  protected readonly members = signal<User[]>([]);
 
   async ngOnInit() {
     this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
     } catch (error) {
       console.error(error);
       throw error;
