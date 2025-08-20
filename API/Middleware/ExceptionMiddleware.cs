aa4354175e878812fb334d2ce823e97582da6f93
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-20 10:17:28 +0000

    Add Exception Middleware and Buggy Controller for enhanced error handling: implement global exception handling with custom `ExceptionMiddleware`, create `ApiException` class for structured error responses, and add `BuggyController` to test various HTTP responses including unauthorized, not found, server error, and bad request.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Text.Json;
+using API.Errors;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "{Message}", e.Message);
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = env.IsDevelopment()
+                ? new ApiException(context.Response.StatusCode, e.Message, e.StackTrace)
+                : new ApiException(context.Response.StatusCode, e.Message, "Internal server error");
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(response, options);
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
