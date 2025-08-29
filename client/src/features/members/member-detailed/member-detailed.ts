5e5a2f85a17474482c0280bd7e15cd488deb088b
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-08-29 04:44:56 +0000

    Add member profile, photos, and messages components with routing: implement new routes for member details, including profile, photos, and messages; update member-detailed component to support navigation; and create corresponding components for member profile, photos, and messages.

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,23 +1,34 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Member } from '../../../types/member';
 import { MemberService } from '../../../core/services/member-service';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detailed.html',
-  styleUrl: './member-detailed.css'
+  styleUrl: './member-detailed.css',
 })
-export class MemberDetailed implements OnInit{
+export class MemberDetailed implements OnInit {
   private readonly memberService = inject(MemberService);
   private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  protected title = signal<string | undefined>('Profile');
   protected member$?: Observable<Member>;
 
   ngOnInit(): void {
     this.member$ = this.loadMember();
+    this.title.set(this.route.firstChild?.snapshot?.title)
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe({
+        next: () => {
+          this.title.set(this.route.firstChild?.snapshot?.title);
+        },
+      });
   }
 
   loadMember() {
