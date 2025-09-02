5caf9fefcd14bdc1dbfa212580f7856786ac02e2
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-02 11:17:49 +0000

    Add LogUserActivity action filter to track user activity: implement LogUserActivity class for updating LastActive field on authenticated requests, register it in dependency injection, and apply to BaseApiController using ServiceFilter.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,8 +1,10 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-[Route( "api/[controller]" )]
+[ServiceFilter(typeof(LogUserActivity))]
+[Route("api/[controller]")]
 [ApiController]
 public class BaseApiController : ControllerBase
 {
