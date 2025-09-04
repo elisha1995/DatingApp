ab857f24f66636de100c235d48ac49b3260d94e3
Elisha Samuel Gyamfi <elishasamuel01@gmail.com>
2025-09-04 13:37:08 +0000

    Add admin service methods: implement user role management and photo approval functionalities in AdminService, utilizing HttpClient for API interactions.

--- client/src/core/services/admin-service.ts
+++ client/src/core/services/admin-service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Photo } from '../../types/member';
+import { User } from '../../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminService {
-  
+  private readonly baseUrl = environment.apiUrl;
+  private readonly http = inject(HttpClient);
+
+  getUserWithRoles() {
+    return this.http.get<User[]>(this.baseUrl + 'admin/users-with-roles');
+  }
+
+  updateUserRoles(userId: string, roles: string[]) {
+    return this.http.post<string[]>(
+      this.baseUrl + 'admin/edit-roles/' + userId + '?roles=' + roles,
+      {}
+    );
+  }
+
+  getPhotosForApproval() {
+    return this.http.get<Photo[]>(this.baseUrl + 'admin/photos-to-moderate');
+  }
+
+  approvePhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/approve-photo/' + photoId, {});
+  }
+
+  rejectPhoto(photoId: number) {
+    return this.http.post(this.baseUrl + 'admin/reject-photo/' + photoId, {});
+  }
 }
